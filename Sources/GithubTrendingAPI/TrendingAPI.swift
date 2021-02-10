7d372808890a7c6cfcecad0ce8861383ca85d975
elkoiko <tcouacault@gmail.com>
2021-02-10 16:00:57 +0100

    TrendingAPI protocol is public

--- Sources/GithubTrendingAPI/TrendingAPI.swift
+++ Sources/GithubTrendingAPI/TrendingAPI.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol TrendingAPI {
+public protocol TrendingAPI {
   static func makeURL(for item: TrendingItems, language: String, dateRange: DateRange?) -> URL
   static func getRepositories(language: String, dateRange: DateRange?) -> [Repository]
   static func getDevelopers(language: String, dateRange: DateRange?) -> [Developer]
